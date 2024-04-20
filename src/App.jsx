4794d4698cdd321a70509841f83fe036d3123848
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-04-20 11:46:51 +0330

    display order data in order component and add form to new order component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./UI/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Order from "./features/order/Order";
+import Order,{loader as orderLoader} from "./features/order/Order";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./UI/AppLayout";
@@ -22,7 +22,7 @@ const App = () => {
         },
         { path: "/cart", element: <Cart /> },
         { path: "order/new", element: <CreateOrder /> },
-        { path: "/order/:orderId", element: <Order /> },
+        { path: "/order/:orderId", element: <Order />,loader:orderLoader ,errorElement: <Error />},
       ],
     },
   ]);
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,43 @@
 const CreateOrder = () => {
-  return <div>CreateOrder</div>;
+  return (<div>
+    <h2>Ready to order? Let's go!</h2>
+
+    <form>
+      <div>
+        <label>First Name</label>
+        <input type="text" name="customer" required />
+      </div>
+
+      <div>
+        <label>Phone number</label>
+        <div>
+          <input type="tel" name="phone" required />
+        </div>
+      </div>
+
+      <div>
+        <label>Address</label>
+        <div>
+          <input type="text" name="address" required />
+        </div>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="priority"
+          id="priority"
+          // value={withPriority}
+          // onChange={(e) => setWithPriority(e.target.checked)}
+        />
+        <label htmlFor="priority">Want to yo give your order priority?</label>
+      </div>
+
+      <div>
+        <button>Order now</button>
+      </div>
+    </form>
+  </div>);
 };
 
 export default CreateOrder;
